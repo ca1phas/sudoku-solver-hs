63485f11585e88fe43e1a39e878a5b61b7430e2a
Casimir Chiong <casimirchiong@gmail.com>
2023-12-18 21:46:57 +0800

    define func boxes and func chop

--- index.hs
+++ index.hs
@@ -1,5 +1,6 @@
-import Data.List (transpose)
+import Data.List
 
+-- Declarations
 type Value = Char
 
 type Row a = [a]
@@ -7,6 +8,20 @@ type Row a = [a]
 type Matrix a = [Row a]
 
 type Grid = Matrix Value
+
+-- Definitions
+boxsize :: Int
+boxsize = 3
+
+values :: [Char]
+values = ['1' .. '9']
+
+empty :: Char -> Bool
+empty = (== '.')
+
+single :: [a] -> Bool
+single [_] = True
+single _ = False
 
 blank :: Grid
 blank = replicate 9 (replicate 9 '.')
@@ -18,7 +33,15 @@ cols :: Matrix a -> [Row a]
 cols = transpose
 
 boxes :: Matrix a -> [Row a]
-boxes [] = []
+boxes = unpack . map cols . pack
+  where
+    pack = split . map split
+    split = chop boxsize
+    unpack = map concat . concat
+
+chop :: Int -> [a] -> [[a]]
+chop _ [] = []
+chop n xs = take n xs : chop n (drop n xs)
 
 unique :: (Eq a) => [a] -> Bool
 unique [] = True
